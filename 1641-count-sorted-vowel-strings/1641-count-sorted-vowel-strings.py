70b1b283d81aebde9f9f69d23b0ec350ba954f2d
Son of Jongkyun, Park <100128652+jhp2014@users.noreply.github.com>
2023-01-09 19:57:14 +0900

    Time: 36 ms (75.57%), Space: 13.8 MB (96.98%) - LeetHub

--- 1641-count-sorted-vowel-strings/1641-count-sorted-vowel-strings.py
+++ 1641-count-sorted-vowel-strings/1641-count-sorted-vowel-strings.py
@@ -1,15 +1,15 @@
 class Solution:
     def countVowelStrings(self, n: int) -> int:
+        dp = [[0] * (n+1) for _ in range(5)]
         
-        def dfs(depth, val):
-            if depth == 0:
-                return 1
-            
-            result = 0
-            for i in range(val, 0, -1):
-                result += dfs(depth-1, i)
-            
-            return result
+        for i in range(5):
+            dp[i][1] = i+1
+        for i in range(1, n+1):
+            dp[0][i] = 1
         
-        return dfs(n, 5)
-                
+        for i in range(1, 5):
+            for j in range(2, n+1):
+                dp[i][j] = dp[i-1][j] + dp[i][j-1]
+        
+        return dp[4][n]
+            
