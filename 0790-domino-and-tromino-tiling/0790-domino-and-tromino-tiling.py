f661cc77ed9f8bd11b1ab22aeef1c8779a9ae2ec
Son of Jongkyun, Park <100128652+jhp2014@users.noreply.github.com>
2023-01-10 20:49:55 +0900

    Time: 46 ms (28.62%), Space: 14.1 MB (38.63%) - LeetHub

--- 0790-domino-and-tromino-tiling/0790-domino-and-tromino-tiling.py
+++ 0790-domino-and-tromino-tiling/0790-domino-and-tromino-tiling.py
@@ -1,9 +1,10 @@
 class Solution:
     def numTilings(self, n: int) -> int:
         dp = [1,1,2,5]
-        
+        total = sum(dp)
         for i in range(4, n+1):
-            val = sum(dp) * 2 - (dp[i-1] + dp[i-2])
+            val = total * 2 - (dp[i-1] + dp[i-2])
             dp.append(val)
+            total += val
         print(dp)
         return dp[n] % (10**9 + 7)
